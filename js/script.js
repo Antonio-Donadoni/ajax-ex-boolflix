825a5866af305d65d649daa3a4b167148609c448
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-27 14:31:20 +0200

    MILESTONE 1

--- js/script.js
+++ js/script.js
@@ -8,48 +8,35 @@ function addClickButtonListener() {
 function getMovie() {
 
  var query = $("#search-bar").val();
- console.log(query);
 
   $.ajax ({
-   url : 'https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno',
+   url : 'https://api.themoviedb.org/3/search/movie',
    method : 'GET',
-  //  data : {
-  // 'api_key': '8a9865f75eca2ef944ceabef50501298',
-  // 'query': 'matrix'
-  //      },
-   success : function(data, state) {
-
-     var success = data['success'];
-     var movies = data['response'];
-
-     if (success) {
-
-       console.log(movies);
-        // for (var i = 0; i < arr.length; i++) {
-        //
-        //   var album = arr[i];
-        //
-        //   var template = $('#album-template').html();
-        //   var compiled = Handlebars.compile(template);
-        //   var target = $('.cds-container');
-        //   var albumHTML = compiled(album);
-        //
-        //   target.append(albumHTML);
-        // }
-
-         }
-
-     else {
-       console.log("error");
-     }
+   data : {
+    'api_key': '8a9865f75eca2ef944ceabef50501298',
+    'query': query,
+    'language': "it_IT"
    },
+
+   success: function(data) {
+     var moviesArray = data["results"]
+
+     for (var i = 0; i < moviesArray.length; i++) {
+
+        var movie = moviesArray[i];
+        var template = $('#movie-template').html();
+        var compiled = Handlebars.compile(template);
+        var target = $('#movies-list');
+        var movieHTML = compiled(movie);
+        target.append(movieHTML);
+      }
+    },
+
    error: function(request, state, error) {
      console.log('request' , request);
      console.log('state' , state);
      console.log('error' , error);
-   }
-
-
+    }
  });
 };
 
